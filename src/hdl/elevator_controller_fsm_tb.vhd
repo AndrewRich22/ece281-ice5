138a4b13629f1e337cb636a5d9f6ba6f5382fdc9
AndrewRich22 <github noreply email@users.noreply.github.com>
2024-04-09 13:58:19 -0600

    Updated tests

--- src/hdl/elevator_controller_fsm_tb.vhd
+++ src/hdl/elevator_controller_fsm_tb.vhd
@@ -118,8 +118,10 @@ begin
              assert w_floor = "0100" report "bad up from floor3" severity failure;  
         w_stop <= '1';  wait for k_clk_period * 2;
               assert w_floor = "0100" report "bad wait on floor4" severity failure;
-        w_stop <= '0'; w_up_down <= '0';
-        wait for k_clk_period;
+        --still on floor 4
+        w_stop <= '0'; wait for k_clk_period * 2;
+            assert w_floor = "0100" report "bad up from floor 4" severity failure;
+       w_up_down <= '0'; wait for k_clk_period * 2;
               assert w_floor = "0011" report "bad down from floor4" severity failure; 
         w_stop <= '1';  wait for k_clk_period * 2;
               assert w_floor = "0011" report "bad wait on floor3" severity failure;
